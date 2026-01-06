27d6d268d88f49bc353b690804d30ed036959543
anish1A1 <alloozpat87@gmail.com>
2026-01-06 10:02:24 +0545

    Understood how Text Loader Works

--- 1_TextLoader.py
+++ 1_TextLoader.py
@@ -1,19 +1,34 @@
 from langchain_community.document_loaders import TextLoader
 from langchain_huggingface import ChatHuggingFace, HuggingFaceEndpoint
 from langchain_core.prompts import PromptTemplate
+from langchain_core.output_parsers import StrOutputParser
+
 
 txt_loader = TextLoader('cricket.txt', encoding='utf-8')
+
 docs = txt_loader.load()
+
 llm = HuggingFaceEndpoint(
     repo_id= "moonshotai/Kimi-K2-Thinking",
     task="text-generation"
 )
+model = ChatHuggingFace(llm=llm)
+
+prompt = PromptTemplate(
+    template='Write a summary for the following poem - \n {poem}',
+    input_variables=['poem']
+)
+
+parser = StrOutputParser()
+
+chain = prompt | model | parser
+output = chain.invoke({'poem': docs[0].page_content})
 
 print(docs)
 # print(docs[0])
 
-
-
 print("\n")
 print(type(docs))
 print(len(docs))
+print("\n")
+print(output)
