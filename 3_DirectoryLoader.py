37ef95ae62d58408301babd9722afdb7ad4fc8d7
anish1A1 <alloozpat87@gmail.com>
2026-01-07 09:31:36 +0545

    learnt to use DirectoryLoader

--- 3_DirectoryLoader.py
+++ 3_DirectoryLoader.py
@@ -1,4 +1,17 @@
 from langchain_community.document_loaders import DirectoryLoader, PyPDFLoader
 
+loader = DirectoryLoader(
+    path='books',
+    glob="*.pdf",
+    loader_cls=PyPDFLoader
+)
+
+docs = loader.load()
+# print(docs)
+print(docs[0])
+print("\n",docs[3].page_content)
+print("\n", docs[348].metadata)
 
 
+
+print("\n Total length/pages of all pdfs", len(docs))
